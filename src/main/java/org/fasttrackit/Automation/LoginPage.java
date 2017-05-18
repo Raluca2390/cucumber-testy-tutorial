9e27e9c2372cfbad915462a5f9c979f7df1e42e7
Serbanescu <ralucang@gmail.com>
2017-05-18 19:25:41 +0300

    11

---
+++ src/main/java/org/fasttrackit/Automation/LoginPage.java
@@ -0,0 +1,28 @@
+package org.fasttrackit.Automation;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by Serbanescu on 18.05.2017.
+ */
+public class LoginPage {
+    @FindBy(name = "username")
+    public WebElement userNameElement;
+
+    @FindBy(id = "password")
+    public WebElement passwordElement;
+
+    @FindBy(id = "loginButton")
+    public WebElement loginBtn;
+
+
+  /*  public static void main(String[] args) {
+        LoginPage loginPage = new LoginPage()
+
+    }*/
+
+
+
+
+}
