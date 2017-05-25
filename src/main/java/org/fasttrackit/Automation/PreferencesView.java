cc9cad7adbc8ec7dd4fd26e529f4f1a7687c40a5
Serbanescu <ralucang@gmail.com>
2017-05-25 20:32:10 +0300

    new version

--- src/main/java/org/fasttrackit/Automation/PreferencesView.java
+++ src/main/java/org/fasttrackit/Automation/PreferencesView.java
@@ -14,49 +14,44 @@ public class PreferencesView {
 
     private Button preferencesButton = new Button().setElCssSelector(".navbar-header button");
 
+    private WebLocator win = new WebLocator().setId("preferences-win");
 
-    private WebLocator xBtn = new WebLocator().setElCssSelector("#preferences-win button.close");
+    private WebLocator xBtn = new Button(win).setClasses("button.close");
 
-    private WebLocator statusMsg = new WebLocator().setElPath("//*[@id='preferences-win']//*[@class='status-msg']") ;
+    //private TextField passElement = new TextField(win).setElPath("//input[@name='password']");
+    private TextField passElement = new TextField(win).setName("password");
 
-    private TextField passElement = new TextField().setElPath("//*[@id='preferences-win']//input[@name='password']");
+    private TextField newPasswordElement = new TextField(win).setName("newPassword");
 
-    private TextField newPasswordElement = new TextField().setElPath("//*[@id='preferences-win']//input[@name='newPassword']");
+    private TextField newPasswordRepeatElement = new TextField(win).setName("newPasswordRepeat");
 
-    private TextField newPasswordRepeatElement = new TextField().setElPath("//*[@id='preferences-win']//input[@name='newPasswordRepeat']");
+    private WebLocator statusMsg = new WebLocator(win).setClasses("status-msg");
 
-    private Button saveBtn = new Button().setElPath("//*[@id='preferences-win']//button[text()='Save']") ;
+   // private Button saveBtn = new Button(win).setElPath("//button[text()='Save']");
+   private Button saveBtn = new Button(win).setText("Save");
 
-    private Button closeBtn = new Button().setElCssSelector("#preferences-win button.close");
+    //private Button closeBtn = new Button(win).setElCssSelector(".modal-footer button");
 
+    private Button closeBtn = new Button(win).setText("Close");
 
-
-
-    public void changePassword (String pass, String newPassword, String newPasswordRepeat) {
-    passElement.sendKeys(pass);
-    newPasswordElement.sendKeys(newPassword);
-    newPasswordRepeatElement.sendKeys(newPasswordRepeat);
-    saveBtn.click();
+    public void changePassword(String pass, String newPassword, String newPasswordRepeat) {
+        passElement.sendKeys(pass);
+        newPasswordElement.sendKeys(newPassword);
+        newPasswordRepeatElement.sendKeys(newPasswordRepeat);
+        saveBtn.click();
     }
 
-    public void Open(){
-      preferencesButton.click();
-    }
-
-    public void Close(){
-
+    public void open() {
+        preferencesButton.click();
         Utils.sleep(400);
+    }
+
+    public void close() {
+
+
         closeBtn.click();
         Utils.sleep(400);
     }
-
-
-
-
-
-
-
-
 
 
 }
